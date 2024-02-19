0267e3552bc11aa3d2cf1b44d06898b03986bb6a
raymundocm240@gmail.com <HP@192.168.0.27>
2024-02-18 21:23:48 -0600

    Usuario y Video

--- src/main/java/com/mycompany/youtube/Youtube.java
+++ src/main/java/com/mycompany/youtube/Youtube.java
@@ -11,9 +11,15 @@ package com.mycompany.youtube;
 public class Youtube {
 
     public static void main(String[] args) {
-        Usuario usuario = new Usuario("limon");
-        Video video = new Video ("aprendiendo java", 2400,"http;//limom.co" );
+        Usuario usuario = new Usuario("rojo");
+        Video video = new Video ("aprendiendo java", 2400,"http;//rojo.co" );
         usuario.subirVideo(video);
         usuario.mostrarInfoUsuario();
+
+        
+        Usuario usuario2=new Usuario("La Rana Rene");
+        Comentario comment1=new Comentario("ta feo el video mi chavo ",usuario2);
+        System.out.println("El usuario "+ usuario2.getNombre()+ "  publicó el comentario "+ comment1.getContenido() );
+
     }
 }
